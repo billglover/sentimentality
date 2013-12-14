55321c9f8ecf50a82595895100625acf39560758
Bill Glover <bill@billglover.co.uk>
2013-12-14 19:50:35 +0000

    Added HTTP calls to toggle the lights based on sentiment score

--- app.js
+++ app.js
@@ -10,52 +10,89 @@ var util       = require('util'),
 
 // use configuration from: parameters, environment variables, and then config.json
 nconf.argv()
-       .env()
-       .file({ file: 'config.json' });
+     .env()
+     .file({ file: 'config.json' });
 
 // set up an array to store the last 'n' scores for a rolling average
 var lastn = fa.newFixedValueHistory(100);
 
-// Set up Twitter client
-console.log();
-console.log('# oAuth Configuration');
-console.log('oauth:consumer_key ' + nconf.get('oauth:consumer_key'));
-console.log('oauth:consumer_secret ' + nconf.get('oauth:consumer_secret'));
-console.log('oauth:access_token_key ' + nconf.get('oauth:access_token_key'));
-console.log('oauth:access_token_secret ' + nconf.get('oauth:access_token_secret'));
+// set up the lights
+var setLights = function setLights(lights) {
+  url = "http://" 
+    + nconf.get('lights:user') 
+    + ":" + nconf.get('lights:password') 
+    + "@" + nconf.get('lights:hostname')
+    + "/Set.cmd?CMD=SetPower+p61=" + lights.red 
+    + "+p62=" + lights.green 
+    + "+p63=0+p64=0";
+  //console.log(url);
 
+  http.get(url, function(res) {
+    //console.log("Got result: " + res);  
+  }).on('error', function(e) {
+    //console.log("Got error: " + e.message);
+  });
+};
+
+// some helpful light control functions
+var toggleRed = function toggleRed(lights) {
+  lights.red = (lights.red == 0 ? 1 : 0);
+  setLights(lights);
+};
+
+var toggleGreen = function toggleRed(lights) {
+  lights.green = (lights.green == 0 ? 1 : 0);
+  setLights(lights);
+};
+
+var toggleBoth = function toggleRed(lights) {
+  lights.red = (lights.red == 0 ? 1 : 0);
+  lights.green = (lights.green == 0 ? 1 : 0);
+  setLights(lights);
+};
+
+// turn the lights off initially
+var lights = {red: 0, green: 0};
+setLights(lights);
+
+// Set up Twitter client
 var twit = new twitter({
-    consumer_key: nconf.get('oauth:consumer_key'),
-    consumer_secret: nconf.get('oauth:consumer_secret'),
-    access_token_key: nconf.get('oauth:access_token_key'),
-    access_token_secret: nconf.get('oauth:access_token_secret')
+  consumer_key: nconf.get('oauth:consumer_key'),
+  consumer_secret: nconf.get('oauth:consumer_secret'),
+  access_token_key: nconf.get('oauth:access_token_key'),
+  access_token_secret: nconf.get('oauth:access_token_secret')
 });
 
 // Handle each tweet as it is received
 var processTweet = function processTweet(tweet) {
 
-    // We only handle English tweets at the moment
-    if(tweet.lang === 'en') {
-        var score = analyze(tweet.text).comparative;
-        lastn.push(score);
+  // We only handle English tweets at the moment
+  if(tweet.lang === 'en') {
+    var score = analyze(tweet.text).comparative;
+    lastn.push(score);
 
-        console.log();
-        console.log(tweet.user.name + ': ' + tweet.text );
-        console.log('score: ' + score);
-        console.log('rolling avg: ' + lastn.mean());
-    };
+    console.log();
+    console.log(tweet.user.name + ': ' + tweet.text );
+    console.log('score: ' + score);
+    console.log('rolling avg: ' + lastn.mean());
+
+    // toggle green for positive, red for negative, or both for indiference
+    if ( score > 0 ) {
+        toggleGreen(lights);
+    } else if ( score < 0 ) {
+        toggleRed(lights);
+    } else {
+        toggleBoth(lights);
+    }
+  };
 };
 
 // Start listening to Streaming API
-console.log();
-console.log('# Stream Filters');
-console.log('track ' + nconf.get('track'));
-
 twit.stream('filter', {track: nconf.get('track')}, function(stream) {
-    stream.on('data', processTweet);
+  stream.on('data', processTweet);
 
-    // If `duration` is set then set the timeout
-    if(nconf.get('duration')) {
-        setTimeout(stream.destroy, nconf.get('duration'));
-    };
+  // If `duration` is set then set the timeout
+  if(nconf.get('duration')) {
+    setTimeout(stream.destroy, nconf.get('duration'));
+  };
 });
